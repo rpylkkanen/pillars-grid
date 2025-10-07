95677919a55926d7936a95c0bc9ccb02e1b388cb
rpylkkanen <robert.pylkkanen@gmail.com>
2025-10-07 10:46:11 +0300

    index

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, send_from_directory
 from flask_cors import CORS
 import trimesh
 import io
